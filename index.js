0b6ad265588fa8005efe4f67d390f0773ec0dff7
Sanyaolu123 <sanyaoluabideen@gmail.com>
2022-03-23 11:14:18 +0100

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+
+const mongoose = require("mongoose");
 const server = require("http").Server(app);
 const router = require("./server/routers/routes");
+
+mongoose.connect(process.env.MONGODB_URI)
 
 app.use("/", router);
 
