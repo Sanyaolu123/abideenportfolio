d6ca975fc515c4e8f78b61c83cbe07e6706ee069
Sanyaolu123 <sanyaoluabideen@gmail.com>
2022-03-23 12:06:03 +0100

    updated

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const mongoose = require("mongoose");
 const server = require("http").Server(app);
 const router = require("./server/routers/routes");
 
+require("dotenv").config()
+
 mongoose.connect(process.env.MONGODB_URI)
 
 app.use("/", router);
 
-server.listen(process.env.PORT)
+server.listen(process.env.PORT || 5000)
--- server/routers/routes.js
+++ server/routers/routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controller");
 
-router.get("/", controller.Home)
+router.get("/api", controller.Home)
 
 module.exports = router;
