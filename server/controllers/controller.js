e142c83391fe261089747bf5ba9e7e91af16b243
Sanyaolu123 <sanyaoluabideen@gmail.com>
2022-03-24 16:22:38 +0100

    updated

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,9 +1,40 @@
-const express = require("express");
+const nodemailer = require('nodemailer');
+const emailValidator = require('deep-email-validator');
+
+async function isEmailValid(email) {
+  return emailValidator.validate(email)
+ }
 
 exports.AddContact = (req, res) => {
   let name = req.body.name;
   let email = req.body.email;
   let message = req.body.message;
 
-  res.send(`${name} which email is ${email} send this message: ${message}`);
+  // res.send(`${name} which email is ${email} send this message: ${message}`);
+  res.status(400).send({message: "Error"})
 }
+
+
+
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: `${process.env.EMAIL}`,
+//     pass: `${process.env.EMAIL_PASS}`
+//   }
+// });
+
+// var mailOptions = {
+//   from: 'youremail@gmail.com',
+//   to: 'myfriend@yahoo.com',
+//   subject: 'Sending Email using Node.js',
+//   text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
